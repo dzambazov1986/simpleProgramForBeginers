a93f409ae5741990efa27c867245d63900c739cf
Dimitar Dzhambazov <129491664+dzambazov1986@users.noreply.github.com>
2024-01-22 11:00:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿double number1 = double.Parse(Console.ReadLine());
+double number2 = double.Parse(Console.ReadLine());
+double number3 = double.Parse(Console.ReadLine());
+
+Console.WriteLine(number3);
+Console.WriteLine(number2);
+Console.WriteLine(number1);
