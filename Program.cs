b01dfa5bd8feedf2c40d520f59e77cca4911c6a7
Dimitar Dzhambazov <129491664+dzambazov1986@users.noreply.github.com>
2024-01-22 11:03:59 +0200

    P4

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
-﻿int requiredNylon = int.Parse(Console.ReadLine());
-int requiredPaint = int.Parse(Console.ReadLine());
-int quantityThiner = int.Parse(Console.ReadLine());
-int hoursNeedCraftsmen = int.Parse(Console.ReadLine());
+﻿int numberOfChicken = int.Parse(Console.ReadLine());
+int numberOfFish = int.Parse(Console.ReadLine());   
+int vegetarianMenu = int.Parse(Console.ReadLine());
 
-double nylon = (requiredNylon + 2) * 1.50;
-double paint = (requiredPaint + 0.1 * requiredPaint) * 14.50;
-int thiner = quantityThiner * 5;
-double bags = 0.40;
+double priceChickenMenu = numberOfChicken * 10.35;
+double priceFishMenu = numberOfFish * 12.40;
+double priceVegetarianMenu = vegetarianMenu * 8.15;
 
-double totalAmountMaterials = nylon + paint + thiner + bags;
-double amountCraftsmen = (totalAmountMaterials * 0.3 ) * hoursNeedCraftsmen;
-double totalAmount = (totalAmountMaterials + amountCraftsmen);
+double totalCostMenus = priceChickenMenu + priceFishMenu + priceVegetarianMenu;
+double priceForDesert = 0.2 * totalCostMenus;
+double deliveryPrice = 2.50;
 
-Console.WriteLine(totalAmount);
+double totalOrderPrice = totalCostMenus + priceForDesert + deliveryPrice;
+
+Console.WriteLine(totalOrderPrice);
